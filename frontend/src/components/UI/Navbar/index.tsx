52e07965cf0c020a7fbc8845557046b5661dd972
Salvatore  Dininni <salvatore.dininni@cienneffe.com>
2022-09-13 16:23:17 +0200

    implementazioni relative alla navbar

--- frontend/src/components/UI/Navbar/index.tsx
+++ frontend/src/components/UI/Navbar/index.tsx
@@ -5,7 +5,7 @@ import classes from './style.module.css';
 const Navbar = () => {
     return (
         <nav className={`flex justify-around items-center sticky top-0 z-20 ${classes['navbar-custom']}`}>
-            <ul className='md:static fixed bottom-0 top-12 md:flex justify-between items-center md:bg- w-10/12 bg-gray-500 text-white space-x-7 md:text-gray-700 md:space-y-0 space-y-5 p-2'>
+            <ul className='md:static fixed bottom-0 top-12 md:flex justify-between items-center md:bg-transparent w-10/12 bg-emerald-500 text-white space-x-7 md:text-gray-700 md:space-y-0 space-y-5 p-2'>
                 <a href="https://www.cienneffe.com/" target='blank' className=" transparent">
                     <img src={Logo} alt="logo cienneffe" />
                 </a>
