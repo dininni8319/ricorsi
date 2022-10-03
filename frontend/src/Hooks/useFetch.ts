3f54b56fd47d8be10affb57dcca49fe2782af850
Salvatore Dininni <s.dininni@yahoo.com>
2022-10-03 17:52:17 +0200

    fasi form create implementation

---
+++ frontend/src/Hooks/useFetch.ts
@@ -0,0 +1,37 @@
+import { useState, useEffect, useCallback } from "react";
+import { Methods } from "../components/interfaces/interfaces";
+import axios from "axios";
+
+const useFetch =  ( endpoint: string, { verb = 'get', params = {} } = {}) => {
+  
+  const [ data, setData] = useState({})
+  const [ loading, setLoading] = useState(false)
+  const [ error, setError ] = useState(null)
+  
+  useEffect(() => {
+
+    axios[verb as Methods](endpoint, params)
+      .then(response => {
+        
+        setData(response.data)
+        
+      })
+      .catch((error) =>{
+        setError(error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+
+  },[endpoint])
+  
+  
+  return {
+  
+    data, 
+    loading,
+    error,
+  }
+}
+
+export default useFetch
--- frontend/src/components/Views/RicorsiDetail/index.tsx
+++ frontend/src/components/Views/RicorsiDetail/index.tsx
@@ -3,7 +3,7 @@ import { RicorsoProps } from "../../interfaces/interfaces";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from 'react-router';
 import { baseURL } from "../../Utilities/index";
-import useApiRequest from '../../state/useApiRequest';
+import useFetch from "../../../Hooks/useFetch";
 import { DetailStyleComponent  } from "./style";
 import DetailPage from '../../UI/DetailRicorsi';
 
@@ -11,40 +11,25 @@ const RicorsiDetail = () => {
   let { slug } = useParams();
   let navigate = useNavigate()
 
-  const [ { status, response }, makeRequest ] = useApiRequest(
-    `${baseURL}/api/cienneffe/detail_ricorso/${slug}`, {
-        verb: 'get',      
-    }
-  )
-
-  const [ { status2, response2 }, makeRequest2 ] = useApiRequest(
-    `${baseURL}/api/cienneffe/ricorso/delete/${slug}`, {
-        verb: 'delete',
-    }
-  )
-
+  const { data } = useFetch(`${baseURL}/api/cienneffe/detail_ricorso/${slug}`, {
+    verb: 'get',      
+  })
+  
+ 
   const handleDelete = (e:any) => {
     e.preventDefault();
-    makeRequest2();
-    navigate('/');
+  
   } 
-
-  useEffect(() => {
-    makeRequest(); 
-  },[])
-//   console.log(response, 'testing the type of ricorso');
-
-  let ricorso = {...response?.data?.ricorso}
   
   return (
       <section className="height-custom flex justify-center">
             <DetailStyleComponent>
                 <h1 className="mb-3 text-center pr-2">Tributo:{ricorso?.tributo}</h1>
 
-                 <DetailPage 
+              {ricorso &&  <DetailPage 
                    ricorso={ricorso}
-                 />
-                <section className='md:px-3'>
+                 />}
+              {ricorso &&  <section className='md:px-3'>
                     <div className='md:flex justify-between  border-bottom-style py-3'>
                         <Link to={`/work_flow/${ricorso.id}`}>Aggiorna Ricorso</Link>
                         <button onClick={handleDelete} className='bg-red-500 text-white outline-none cursor-pointer w-18 px-3 py-2 font-semibold'>Cancella</button>
@@ -52,7 +37,7 @@ const RicorsiDetail = () => {
                     <div className="card-actions">
                          <p className='font-serif mt-3'>{ricorso.oggetto_ricorso}</p>
                     </div>
-                </section>
+                </section>}
             </DetailStyleComponent>
         </section>
       
