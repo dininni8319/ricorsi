bbfd5f41825884ce1151edfecef457b4f1fac41a
Salvatore Dininni <s.dininni@yahoo.com>
2022-10-06 16:33:26 +0200

    implemented lazy loading in thee frontend

--- frontend/src/components/Views/App/App.tsx
+++ frontend/src/components/Views/App/App.tsx
@@ -1,23 +1,70 @@
 import './App.css';
+import { lazy, Suspense} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Homepage, Ricorsi, Cartoline, Fase, RicorsiDetail, FaseDetail } from '../index';
-import { Navbar, Footer, Header } from "../../UI/index";
+// import { Homepage, Ricorsi, Cartoline, Fase, RicorsiDetail, FaseDetail } from '../index';
+import { Navbar, Footer, Header, Loader3 } from "../../UI/index";
 
 const App = () => {
+  
+  const Homepage = lazy(() => import('../Home/Homepage'));
+  const Ricorsi = lazy(() => import('../RicorsiWorkflow'));
+  const Cartoline = lazy(() => import('../Cartoline'));
+  const Fase = lazy(() => import('../Fase'));
+  const RicorsiDetail = lazy(() => import('../RicorsiDetail'));
+  const FaseDetail = lazy(() => import('../FaseDetail'));
 
   return (
     <BrowserRouter>
         <Navbar />
         <Header/>
         <Routes>
-            <Route path='/' element={<Homepage />}/>
-            <Route path='/ricorsi' element={<Ricorsi />}/>
-            <Route path='/ricorsi/:slug' element={<Ricorsi />}/>
-            <Route path='/ricorsi_detail/:slug' element={<RicorsiDetail />} />
-            <Route path='/form_fase/:slug' element={<Fase />} />
-            <Route path='/fase_detail/:slug' element={<FaseDetail />} />
-            <Route path='/work_flow/' element={<Cartoline />} />
-            <Route path='/work_flow/:slug' element={<Cartoline />} />
+          <Route path='/' element={
+            <Suspense fallback={<Loader3/>}>
+               <Homepage />
+            </Suspense> 
+          }/>
+
+          <Route path='/ricorsi' element={
+            <Suspense fallback={<Loader3/>}>
+               <Ricorsi />
+            </Suspense> 
+          }/>
+
+          <Route path='/ricorsi/:slug' element={
+            <Suspense fallback={<Loader3/>}>
+               <Ricorsi />
+            </Suspense> 
+          }/>
+
+          <Route path='/work_flow/' element={
+            <Suspense fallback={<Loader3/>}>
+               <Cartoline />
+            </Suspense> 
+          }/>
+
+          <Route path='/work_flow/:slug' element={
+            <Suspense fallback={<Loader3/>}>
+               <Cartoline />
+            </Suspense> 
+          }/>
+
+          <Route path='/form_fase/:slug' element={
+            <Suspense fallback={<Loader3/>}>
+               <Fase />
+            </Suspense> 
+          }/>
+
+          <Route path='/ricorsi_detail/:slug' element={
+            <Suspense fallback={<Loader3/>}>
+               <RicorsiDetail />
+            </Suspense> 
+          }/>
+         
+          <Route path='/fase_detail/:slug' element={
+            <Suspense fallback={<Loader3/>}>
+               <FaseDetail />
+            </Suspense> 
+          }/>
         </Routes>
         <Footer />
     </BrowserRouter>
--- frontend/src/components/Views/Home/Homepage.tsx
+++ frontend/src/components/Views/Home/Homepage.tsx
@@ -3,21 +3,20 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { baseURL } from "../../Utilities/index";
 import { ObjFormType } from "../../interfaces/interfaces";
-import { Card, Aside, Loader3 } from "../../UI/index";
+ import { Card } from "../../UI/index";
 import { WrapperStyleComponent } from "./style";
 import useFetch from "../../../Hooks/useFetch";
 import useApiRequest from '../../state/useApiRequest';
 
 const Homepage = () => {
     const [ ricorsi, setRicorsi ] = useState<{[key: string]: string}[]>([])
-
-      useEffect(() => {
-        axios.get(`${baseURL}/api/cienneffe/ricorsi`)
-          .then(response => {
-            setRicorsi(response.data.ricorsi);
-          })
-          .catch((error: unknown) =>{
     
+    useEffect(() => {
+        fetch(`${baseURL}/api/cienneffe/ricorsi`)
+          .then(response => response.json())
+          .then(data => setRicorsi(data.ricorsi))
+          .catch((error: unknown) =>{
+             console.log(error);
           })
       },[])
     
@@ -27,43 +26,46 @@ const Homepage = () => {
                 <> 
                     {ricorsi?.map((ricorso, id: number) => {
                         return (
-                            <Card 
-                                taxunit={ricorso}
-                                key={id}
-                                path='ricorso/delete'
-                                current={ricorsi}
-                                setCurrent={setRicorsi}
-                            >
-                                <>
-                                    <h3 className="card-title mb-3">Tributo: <span>{ricorso.tributo}</span></h3>
+                            <>
 
-                                    <ul className="border-custom ul-style-custom">
-                                        <li>
-                                            <span className="font-semibold pr-1">Numero Ricorso:</span>{ricorso.numero_ricorso}
-                                        </li>
-                                        <li>
-                                            <span className="font-semibold pr-1">Ente:</span>{ricorso.ente} 
-                                        </li>
-                                        <li>
-                                            <span className="font-semibold pr-1">Anno imposta:</span>{ricorso.anno_imposta}
-                                        </li>
-                                        <li>
-                                            <span className="font-semibold pr-1">Importo Atto:</span>{ricorso.importo_atto}
-                                        </li>
-                                        <li>
-                                            <span className="font-semibold pr-1">Esito:</span>{ricorso.esito}
-                                        </li>
-                                        <li>
-                                            <p className='font-serif text-sm'><span className="font-semibold pr-1">Descrizione:</span>{ricorso.oggetto_ricorso}</p>
-                                        </li>
-                                    </ul>
+                                <Card 
+                                    taxunit={ricorso}
+                                    key={id}
+                                    path='ricorso/delete'
+                                    current={ricorsi}
+                                    setCurrent={setRicorsi}
+                                >
+                                    <>
+                                        <h3 className="card-title mb-3">Tributo: <span>{ricorso.tributo}</span></h3>
 
-                                    <div className='flex justify-between py-1'>
-                                        <Link to={`/ricorsi/${ricorso.id}`}>Aggiorna Ricorso</Link>
-                                        <Link to={`/ricorsi_detail/${ricorso.id}`}>Dettaglio Ricorso</Link>
-                                    </div>
-                                </>
-                            </Card>
+                                        <ul className="border-custom ul-style-custom">
+                                            <li>
+                                                <span className="font-semibold pr-1">Numero Ricorso:</span>{ricorso.numero_ricorso}
+                                            </li>
+                                            <li>
+                                                <span className="font-semibold pr-1">Ente:</span>{ricorso.ente} 
+                                            </li>
+                                            <li>
+                                                <span className="font-semibold pr-1">Anno imposta:</span>{ricorso.anno_imposta}
+                                            </li>
+                                            <li>
+                                                <span className="font-semibold pr-1">Importo Atto:</span>{ricorso.importo_atto}
+                                            </li>
+                                            <li>
+                                                <span className="font-semibold pr-1">Esito:</span>{ricorso.esito}
+                                            </li>
+                                            <li>
+                                                <p className='font-serif text-sm'><span className="font-semibold pr-1">Descrizione:</span>{ricorso.oggetto_ricorso}</p>
+                                            </li>
+                                        </ul>
+
+                                        <div className='flex justify-between py-1'>
+                                            <Link to={`/ricorsi/${ricorso.id}`}>Aggiorna Ricorso</Link>
+                                            <Link to={`/ricorsi_detail/${ricorso.id}`}>Dettaglio Ricorso</Link>
+                                        </div>
+                                    </>
+                                </Card>
+                            </>
                         )
                     })}
                 </>
