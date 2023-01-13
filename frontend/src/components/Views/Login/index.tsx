f05a9e604fb61ecc561acf9beda6d78ec7793843
Salvatore Dininni <s.dininni@yahoo.com>
2023-01-13 10:58:41 +0100

    testando il login

--- frontend/src/components/Views/Login/index.tsx
+++ frontend/src/components/Views/Login/index.tsx
@@ -20,7 +20,7 @@ const Login = () => {
     const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        fetch(`${backend}/api/cienneffe/login`, {
+        fetch(`http://127.0.0.1:8000/api/cienneffe/login`, {
             method: 'POST',
             headers: { 'Content-type': 'application/json' },
             body: JSON.stringify(data)
